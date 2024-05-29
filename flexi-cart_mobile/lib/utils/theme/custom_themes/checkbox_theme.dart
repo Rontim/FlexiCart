4bd02ed29fb17875681f67af8044081d7e575d5b
Rontim <rongitonga@gmail.com>
2024-05-29 23:43:40 +0300

    Doc: Updated the checkbox theme data

--- flexi-cart_mobile/lib/utils/theme/custom_themes/checkbox_theme.dart
+++ flexi-cart_mobile/lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -2,37 +2,41 @@ import 'package:flexi_cart_mobile/utils/constants/colors.dart';
 import 'package:flexi_cart_mobile/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
+/// Custom Class for Light & Dark Text Themes
 class FCheckboxTheme {
-  FCheckboxTheme._();
+  FCheckboxTheme._(); // To avoid creating instances
 
+  /// Customizable Light Text Theme
   static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(FSizes.xs)),
-    checkColor: WidgetStateColor.resolveWith((states) {
-      if (states.contains(WidgetState.disabled)) {
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
         return FColors.white;
       } else {
         return FColors.black;
       }
     }),
-    fillColor: WidgetStateColor.resolveWith((states) {
-      if (states.contains(WidgetState.disabled)) {
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
         return FColors.primaryColor;
       } else {
         return Colors.transparent;
       }
     }),
   );
+
+  /// Customizable Dark Text Theme
   static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(FSizes.xs)),
-    checkColor: WidgetStateColor.resolveWith((states) {
-      if (states.contains(WidgetState.disabled)) {
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
         return FColors.white;
       } else {
         return FColors.black;
       }
     }),
-    fillColor: WidgetStateColor.resolveWith((states) {
-      if (states.contains(WidgetState.disabled)) {
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
         return FColors.primaryColor;
       } else {
         return Colors.transparent;
