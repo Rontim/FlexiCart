58c1097c31fd27e86d7f332e0bcee3cd7fcc3782
Rontim <rongitonga@gmail.com>
2024-05-29 10:00:15 +0300

    Feat: Updated the app screen

--- flexi-cart_mobile/lib/app.dart
+++ flexi-cart_mobile/lib/app.dart
@@ -1,16 +1,27 @@
+import 'package:flexi_cart_mobile/utils/constants/text_strings.dart';
 import 'package:flexi_cart_mobile/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FlexiCart',
+    return GetMaterialApp(
+      title: FTexts.appName,
       themeMode: ThemeMode.system,
       theme: FAppTheme.lightTheme,
       darkTheme: FAppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Awesome! 🎊 Project Structure is set up and running. \n Happy T Coding 🎊',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }
